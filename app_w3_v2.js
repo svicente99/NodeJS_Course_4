0d7b37efe9edf019da9e17d942b93a8bfdaae428
Sergio Vicente <svicente99@yahoo.com>
2020-02-09 15:24:27 -0300

    NodeJS Coursera AuthWithSession exercise

--- app_w3_v2.js
+++ app_w3_v2.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
+const Dishes = require("./models/dishes");
+
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-    console.log("------> Connected correctly to server <------");
+    console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
 var createError = require('http-errors');
@@ -14,13 +16,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var userRouter = require('./routes/userRouter');
+var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leadRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
-
-var testRouter = require('./routes/testRouter');
-
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
@@ -30,63 +29,57 @@ var app = express();
 app.use(session({
 	name: 'session-id',
 	secret: '12345-67890-09876-54321',
-	saveUninitialized: false,
+	saveUnitialized: false,
 	resave: false,
     store: new FileStore()
 }));
 
-app.use('/', indexRouter);
-app.use('/users', userRouter);
-
-// Basic Authentication with route user
+// Basic Authentication
 
 function auth (req, res, next) {
   console.log(req.session);
-  console.log("-----auth method result-----");
 
   if(!req.session.user) {
-	console.log("nao autenticado");
-	var err = new Error('You are not authenticated!');
-	err.status = 403;
-	return next(err);
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
   }
-  else{
-	  if( req.session.user === "authenticated" ) {	
-		  console.log("Usuario autenticado");
-	  	  next();
-	  }
-	  else{
-	      console.log("nao autenticado");
-	  	  var err = new Error("You're not authenticated!'");
-          err.status = 403;
-		  return next(err);
-      }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
   }
 }
 
-
-app.use('/test', testRouter);
-
+console.log("using new app with authentication by session");
+app.use(cookieParser('12345-67890-09876-54321'));
+app.use(auth);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('12345-67890-09876-54321'));
-app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
 
-////app.use(logger('dev'));
-////app.use(express.json());
-////app.use(express.urlencoded({ extended: false }));
-////app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leadRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log("Page not found");
   next(createError(404));
 });
 
@@ -100,5 +93,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
---
+++ models/users.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:  {
+        type: String,
+        required: true
+    },
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+var Users = mongoose.model('User', userSchema);
+
+module.exports = Users;
+
